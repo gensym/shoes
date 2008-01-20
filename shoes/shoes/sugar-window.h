9af5df7061525b5943e4b86cd4b681f0a6c668f1
trythil <trythil@nevrast-2.local>
2008-01-19 20:15:15 -0500

    Reworked header guards.

--- shoes/shoes/sugar-window.h
+++ shoes/shoes/sugar-window.h
@@ -1,11 +1,11 @@
 //
-// shoes/olpc.h
+// shoes/sugar-window.h
 // Shoes setup code specific to the Sugar graphical interface,
 // which is used in the OLPC XO-1.
 //
 
-#ifndef SHOES_SUGAR_H
-#define SHOES_SUGAR_H
+#ifndef SHOES_SUGAR_WINDOW_H
+#define SHOES_SUGAR_WINDOW_H
 
 #include "shoes/code.h"
 
