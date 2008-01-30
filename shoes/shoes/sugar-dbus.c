0abb0cbc386945b17f556af41c6279ad6ecdf479
trythil@member.fsf.org <trythil@member.fsf.org>
2008-01-30 13:47:16 -0500

    DBus boilerplate.

--- shoes/shoes/sugar-dbus.c
+++ shoes/shoes/sugar-dbus.c
@@ -6,13 +6,37 @@
 // This file contains code necessary to establish a DBus service
 // for Sugar interprocess communication.
 //
-#include "shoes/sugar-dbus.h"
-
 #include <glib.h>
 #include <dbus/dbus-glib.h>
+
+#include "shoes/sugar-ruby.h"
+#include "shoes/sugar-dbus.h"
+
+static DBusHandlerResult
+shoes_sugar_dbus_filter(DBusConnection *connection, DBusMessage *message, void *user_data);
 
 shoes_code
 shoes_sugar_setup_dbus(shoes_app *app)
 {
+  DBusError error;
+  DBusConnection *bus;
+
+  dbus_error_init(&error);
+  bus = dbus_bus_get(DBUS_BUS_SESSION, &error);
+
+  if (!bus) {
+    dbus_error_free(&error);
+    return SHOES_FAIL;
+  }
+
+  dbus_bus_add_match(bus, "type='signal',interface='org.laptop.Activity'");
+  dbus_connection_add_filter(bus, shoes_sugar_dbus_filter, NULL, NULL);
+
   return SHOES_OK;
+}
+
+static DBusHandlerResult
+shoes_sugar_dbus_filter(DBusConnection *connection, DBusMessage *message, void *user_data)
+{
+  return DBUS_HANDLER_RESULT_NOT_YET_HANDLED;
 }
